aff6dadc5d562234809dd15ce53e47b384ace0bd
Lelik Jan <21alex7777@gmail.com>
2022-06-05 18:42:54 +0300

    feat: process.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 //этот пример демонстрирует работу под капотом 4 потоков по умолчанию библиотеки,
 // т.е. задержка выполнения 5 и 6 криптографических ф-ций связана с тем,что 4 потока заняты,
 // когда они освобождаются, то могут запуститься оставшиеся две ф-ции
-const crypto=require('crypto');
+//этим всем управляет планировщик потоков(thread scheduler )
+/*const crypto=require('crypto');
 const start=Date.now();
 crypto.pbkdf2('123456','5',100000,64,'sha512',()=>{
     console.log('1 end for ',Date.now()-start,'ms');
@@ -20,4 +22,7 @@ crypto.pbkdf2('123456','5',100000,64,'sha512',()=>{
 });
 crypto.pbkdf2('123456','5',100000,64,'sha512',()=>{
     console.log('6 end for ',Date.now()-start,'ms');
-});
+});*/
+
+//console.log(process.pid)
+
---
+++ lessons/process.js
@@ -0,0 +1,18 @@
+//console.log(process.pid);
+//могу создать бесконечный цикл,который создаст процесс в диспетчере задач и под его id его можно увидеть
+// используя command :  kill numberIdprocess его можно остановить
+/*while (true){
+
+}*/
+/*const dotenv=require('dotenv');
+dotenv.config();
+console.log(process.env.PORT)
+console.log(process.env.NODE_ENV)*/
+//console.log(process.argv)
+if(Math.random()>0.5){
+    while (true){
+    }
+}else {
+    console.log('execution of programm finish');
+    process.exit();
+}
