0ac27897b665931514e7909b57492f686d622ad3
Lelik Jan <21alex7777@gmail.com>
2022-06-08 17:20:39 +0300

    feat: file_system.js: different methods

--- lessons/process.js
+++ lessons/process.js
@@ -4,15 +4,18 @@
 /*while (true){
 
 }*/
-/*const dotenv=require('dotenv');
+//получение доступа к глобальным переменным окружения из .env
+const dotenv = require('dotenv');
 dotenv.config();
+
 console.log(process.env.PORT)
-console.log(process.env.NODE_ENV)*/
-//console.log(process.argv)
-if(Math.random()>0.5){
-    while (true){
+console.log(process.env.NODE_ENV)
+//можем ввести что-то при запуске и это отразится в логах в виде массива
+console.log(process.argv)
+if (Math.random() > 0.5) {
+    while (true) {
     }
-}else {
-    console.log('execution of programm finish');
+} else {
+    console.log('execution of program finish');
     process.exit();
 }
