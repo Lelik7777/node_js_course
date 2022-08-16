0209f6946ee1389da09d525d0d9a6a7252c94d2d
Lelik Jan <21alex7777@gmail.com>
2022-08-16 10:58:49 +0300

    feat: add comments to lessons/app.js

--- lessons/app.js
+++ lessons/app.js
@@ -5,6 +5,8 @@ const arg=process.argv;
 console.log(arg);
 //здесь я получаю данные о переменной PORT1 из командной строки,в которой  я передал эту переменную
 // при запуске  node ./lessons/app.js благодаря модулю cross-evn
-console.log(process.env.PORT1)
+console.log('PORT1 from cli: ',process.env.PORT1)
 //здесь же я получаю доступ к файлу .env,где лежат пользовательские переменные(переменные окружения)
-console.log(process.env.PORT)
+console.log('PORT from .env: ',process.env.PORT);
+console.log('TEXT from .env: ',process.env.TEXT);
+console.log('NEW_TEXT from .env: ',process.env.NEW_TEXT);
