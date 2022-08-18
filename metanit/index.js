3ed24c586b97935ef7429906e305c982ddb49f69
Lelik Jan <21alex7777@gmail.com>
2022-08-18 11:57:19 +0300

    feat import data in index.js in metanit from module2

--- metanit/index.js
+++ metanit/index.js
@@ -1,2 +1,6 @@
 const {showName}=require('npm-package');
 showName('bob');
+
+const{date,name}=require('module2');
+console.log('date from module2: ',date);
+console.log('name from module2 : ',name);
